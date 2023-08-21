6daa126224829f95e11eda0822cc38c12f4d4163
Chunyang Shen <scy0208@gmail.com>
2023-08-20 22:42:24 -0700

    Change register-config api

--- src/app/api/v0/register-llm-config/route.ts
+++ src/app/api/v0/register-llm-config/route.ts
@@ -1,9 +1,11 @@
 import { createClient } from '@/utils/supabase';
 
+type JSONType = { [key: string]: any };
+
 type RequestData = {
-    config: string,
+    config: JSONType,
     project_id: string,
-    name: string
+    name: string,
 }
 
 export const runtime = 'edge';
@@ -12,11 +14,11 @@ async function insertConfig(
     { config, project_id, name }: RequestData) {
 
     if (!name) {
-        throw new Error("Id is required");
+        throw new Error("Config name is required");
     }
 
     if (!config) {
-        throw new Error("Content is required");
+        throw new Error("Config content is required");
     }
     if (!project_id) {
         throw new Error("project_id is required");
@@ -29,7 +31,7 @@ async function insertConfig(
     };
 
     const { data, error } = await createClient()
-        .from('llm_Config')
+        .from('model_config')
         .upsert([dataToInsert])
         .select();
 
@@ -37,7 +39,7 @@ async function insertConfig(
         throw new Error(error?.message || "Data is null or empty");
     }
 
-    return data[0].id;
+    return data[0].name;
 }
 
 export async function OPTIONS(req: Request) {
@@ -47,7 +49,14 @@ export async function OPTIONS(req: Request) {
 export async function PUT(req: Request) {
     
     try {
-        const requestData = (await req.json()) as RequestData;
+        let requestData: RequestData;
+
+        try {
+            requestData = await req.json();
+        } catch (parseError) {
+            return new Response(JSON.stringify({ message: 'Invalid JSON payload' }), { status: 400 });
+        }
+
         if (!requestData.project_id) {
             return new Response(JSON.stringify({ message: 'project_id is required' }), { status: 400 });
         }
@@ -60,14 +69,12 @@ export async function PUT(req: Request) {
             return new Response(JSON.stringify({ message: 'Config is required' }), { status: 400 });
         }
 
-        try {
-            JSON.parse(requestData.config);
-        } catch (e) {
-            return new Response(JSON.stringify({ message: 'Invalid JSON content in config' }), { status: 400 });
+        if (typeof requestData.config !== "object" ) {
+            return new Response(JSON.stringify({ message: 'Config must be a valid JSON object' }), { status: 400 });
         }
 
-        const id = await insertConfig(requestData);
-        return new Response(JSON.stringify({ id }), { status: 200 });
+        const name = await insertConfig(requestData);
+        return new Response(JSON.stringify({ name }), { status: 200 });
     } catch (error) {
         console.error("An error occurred:", error);
         return new Response(JSON.stringify({ message: 'Internal Server Error' }), { status: 500 });
