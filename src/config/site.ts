93df9741cef5794632a052bc73d3c0ecde75bd06
Chunyang Shen <scy0208@gmail.com>
2023-08-23 01:13:18 -0700

    update image

--- src/config/site.ts
+++ src/config/site.ts
@@ -5,7 +5,7 @@ export const siteConfig: SiteConfig = {
   description:
     "Effortlessly gather user feedback on AI-generated content and tailor your model to align with user preferences.",
   url: "https://llmfeedback.com/",
-  ogImage: "https://llmfeedback.com/og.png",
+  ogImage: "https://llmfeedback.com/og.jpg",
   links: {
     twitter: "https://twitter.com/CShen1992",
     github: "https://github.com/scy0208/llm-feedback-monitor",
