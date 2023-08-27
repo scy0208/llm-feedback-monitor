da78860ebe38935a595183c558250586fa43f736
Chunyang Shen <scy0208@gmail.com>
2023-08-26 17:32:46 -0700

    Add

---
+++ src/app/(dashboard)/dashboard/monitor/page.tsx
@@ -0,0 +1,84 @@
+import { createClient } from '@/utils/supabase';
+import { Table, Grid, Heading, Section } from '@radix-ui/themes';
+
+import { getCurrentUser } from "@/lib/session"
+import { redirect } from "next/navigation"
+import { authOptions } from "@/lib/auth"
+
+import { Monitor } from "@/components/charts/monitor";
+
+
+export default async function DashboardPage() {
+
+    const user = await getCurrentUser()
+
+    if (!user) {
+        redirect(authOptions?.pages?.signIn || "/login")
+    }
+
+    const { data, error } = await createClient()
+        .from('content')
+        .select('*')
+        .eq('project_id', 'proj_H1VnkAuFsGnaB9eykx_P0');
+
+    const feedbacks1 = [
+        {
+            key: "ThumbUp",
+            totalScore: 109,
+        },
+        {
+            key: "ThumbDown",
+            totalScore: 48,
+        },
+    ];
+
+    const feedbacks2 = [
+        {
+            key: "ThumbUp",
+            totalScore: 3,
+        },
+        {
+            key: "ThumbDown",
+            totalScore: 1,
+        },
+    ];
+
+    return (
+        <>
+            <Heading size="7" className="pb-5">Automatic Bad Answer Detection</Heading>
+            <Grid columns="2" gap="3" width="auto">
+                <div>
+                    <Heading size="5" className="pb-5">General</Heading>
+                    <Monitor/>
+                </div>
+                <div>
+                    <Heading size="5" className="pb-5">Details</Heading>
+                    <Table.Root variant="surface">
+                    <Table.Header>
+                        <Table.Row>
+                            <Table.ColumnHeaderCell>Content</Table.ColumnHeaderCell>
+                            <Table.ColumnHeaderCell>Config Name</Table.ColumnHeaderCell>
+                        </Table.Row>
+                    </Table.Header>
+                    <Table.Body>
+                        {data && data.length > 0 ? (
+                            data.map((item, index) => {
+                                return (
+                                    <Table.Row key={index}>
+                                        <Table.Cell>{item.content}</Table.Cell>
+                                        <Table.Cell>{item.config_name}</Table.Cell>
+                                    </Table.Row>
+                                );
+                            })
+                        ) : (
+                            <Table.Row>
+                                <Table.Cell colSpan={4}>No data available</Table.Cell>
+                            </Table.Row>
+                        )}
+                    </Table.Body>
+                </Table.Root>
+            </div>
+            </Grid>
+        </>
+    );
+}
---
+++ src/components/charts/monitor.tsx
@@ -0,0 +1,86 @@
+'use client';
+
+import { Card, Title, BarChart, Subtitle } from "@tremor/react";
+
+const chartdata = [
+    {
+        name: "2023-08-08",
+        "Bad answer": 8,
+      },
+    {
+        name: "2023-08-09",
+        "Bad answer": 12,
+      },
+    {
+        name: "2023-08-10",
+        "Bad answer": 0,
+      },
+    {
+        name: "2023-08-11",
+        "Bad answer": 1,
+      },
+  {
+    name: "2023-08-12",
+    "Bad answer": 8,
+  },
+  {
+    name: "2023-08-13",
+    "Bad answer": 0,
+  },
+  {
+    name: "2023-08-14",
+    "Bad answer": 7,
+  },
+  {
+    name: "2023-08-15",
+    "Bad answer": 23,
+  },
+  {
+    name: "2023-08-16",
+    "Bad answer": 13,
+  },
+  {
+    name: "2023-08-17",
+    "Bad answer": 18,
+  },
+  {
+    name: "2023-08-18",
+    "Bad answer": 24,
+  },
+  {
+    name: "2023-08-19",
+    "Bad answer": 3,
+  },
+  {
+    name: "2023-08-20",
+    "Bad answer": 0,
+  },
+  {
+    name: "2023-08-21",
+    "Bad answer": 11,
+  },
+];
+
+const dataFormatter = (number: number) => {
+  return Intl.NumberFormat("us").format(number).toString();
+};
+
+export function Monitor() {
+    return (
+  <Card>
+    <Title>Bad answer monitoring</Title>
+    <Subtitle>
+      Bad answer detected
+    </Subtitle>
+    <BarChart
+      className="mt-6"
+      data={chartdata}
+      index="name"
+      categories={["Bad answer"]}
+      colors={["red"]}
+      valueFormatter={dataFormatter}
+      yAxisWidth={48}
+    />
+  </Card>
+    )
+}
