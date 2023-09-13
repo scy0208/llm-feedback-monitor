5d76051eabb0ae2ce8b6b102e8b055848a80f7c5
Chunyang Shen <scy0208@gmail.com>
2023-09-13 12:08:35 -0700

    Change Footer

--- src/components/site-footer.tsx
+++ src/components/site-footer.tsx
@@ -11,25 +11,7 @@ export function SiteFooter({ className }: React.HTMLAttributes<HTMLElement>) {
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
           <Icons.logo />
           <p className="text-center text-sm leading-loose md:text-left">
-            Built by{" "}
-            <a
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              SpringSun Technologies LLC
-            </a>
-            {/* . The source code is available on{" "}
-            <a
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-              className="font-medium underline underline-offset-4"
-            >
-              GitHub
-            </a> */}
-            . Please Follow our{" "}
+            Built by SpringSun Technologies. Please Follow our{" "}
             <a
               href={siteConfig.links.twitter}
               target="_blank"
