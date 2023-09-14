7ee52c91b71fae8cc65e3d90ba9f0082f2a40940
Chunyang Shen <scy0208@gmail.com>
2023-09-14 11:49:16 -0700

    Update OG description

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ import { SiteConfig } from "@/types"
 export const siteConfig: SiteConfig = {
   name: "LLM Feedback",
   description:
-    "Effortlessly gather user feedback on AI-generated content and tailor your model to align with user preferences.",
+    "Easily integrate feedback widget and text analysis module into your Conversational AI application, and evolve it with insights from user interation.",
   url: "https://llmfeedback.com",
   ogImage: "https://d2aaddunp29031.cloudfront.net/llm-config-version.png/og.jpg",
   links: {
