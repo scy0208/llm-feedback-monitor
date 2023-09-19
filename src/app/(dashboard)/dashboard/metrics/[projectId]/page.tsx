8e9f5da403be047baaadd26858e706b6f2f88684
Chunyang Shen <scy0208@gmail.com>
2023-09-19 02:12:51 -0700

    Add fix

--- src/app/(dashboard)/dashboard/metrics/[projectId]/page.tsx
+++ src/app/(dashboard)/dashboard/metrics/[projectId]/page.tsx
@@ -1,7 +1,6 @@
 import { ConfigSelector } from "@/components/config-selector";
 import { MetricsCard } from "@/components/metrics/metricsCard";
 import { Metric, KeyMetrics } from "@/components/metrics/numberCard";
-// import { useState,useEffect } from 'react';
 
 // MetricsPage
 export default async function MetricsPage({ params }: { params: { projectId: string } }) {
