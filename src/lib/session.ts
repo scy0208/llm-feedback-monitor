2082ab61cd3953c82c7c7847cf190f0b04b296cc
Chunyang Shen <scy0208@gmail.com>
2023-09-21 15:11:44 -0700

    add try getCurrentUser

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -3,7 +3,12 @@ import { getServerSession } from "next-auth/next"
 import { authOptions } from "@/lib/auth"
 
 export async function getCurrentUser() {
-  const session = await getServerSession(authOptions)
-
-  return session?.user
+  try {
+    const session = await getServerSession(authOptions);
+    return session?.user;
+  } catch (error) {
+    console.error('Error getting the user session', error);
+    // handle error appropriately, maybe return null or undefined
+    return null;
+  }
 }
