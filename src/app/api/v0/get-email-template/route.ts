102b1fd5637aadb44a4e935663cf71aaabe9eaa2
Chunyang Shen <scy0208@gmail.com>
2023-10-06 03:11:46 -0700

    Add get template

---
+++ src/app/api/v0/get-email-template/route.ts
@@ -0,0 +1,27 @@
+import { createClient } from '@/utils/supabase';
+
+type RequestData = {
+    email: string,
+    label_ids: string[]
+}
+
+async function get_templates(email: string, labelIds: string[]): Promise<any> {
+    let queryBuilder = createClient()
+        .from('email_templates')
+        .select(`template`)
+        .eq('email', email)
+        .in('label_id', labelIds)
+    const { data, error } = await queryBuilder;
+    const res = data?.map((d: any) => d.template)
+    console.log(res);
+    return res;
+}
+
+export async function POST(request: Request) {
+    const { email, label_ids } = (await request.json()) as RequestData
+    const templates = await get_templates(email, label_ids)
+    if (!templates) {
+        return new Response("", { status: 200 })
+    }
+    return new Response(JSON.stringify(templates), { status: 200 })
+}
