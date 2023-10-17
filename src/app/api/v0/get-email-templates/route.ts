3b57e132a6a0514937079d88ed05dd935dd13e33
Chunyang Shen <scy0208@gmail.com>
2023-10-16 20:22:25 -0700

    Fix edge setting and add new API

--- src/app/api/v0/get-email-templates/route.ts
+++ src/app/api/v0/get-email-templates/route.ts
@@ -10,19 +10,31 @@ export const runtime = 'edge';
 async function get_templates(email: string, labelNames: string[]): Promise<any> {
     let queryBuilder = createClient()
         .from('email_templates')
-        .select(`template`)
+        .select('label_name, template') // Added 'label_name' to the selection.
         .eq('email', email)
         .in('label_name', labelNames)
     const { data, error } = await queryBuilder;
-    const res = data?.map((d: any) => d.template)
-    console.log(res);
-    return res;
+    
+    if (!data) return {};
+
+    // Aggregating results by 'label_name'
+    let groupedTemplates: { [labelName: string]: string[] } = {};
+    for (const entry of data) {
+        if (entry.template !== null) { // This line filters out the null templates
+            if (!groupedTemplates[entry.label_name]) {
+                groupedTemplates[entry.label_name] = [];
+            }
+            groupedTemplates[entry.label_name].push(entry.template);
+        }
+    }
+    console.log(groupedTemplates);
+    return groupedTemplates;
 }
 
 export async function POST(request: Request) {
     const { email, labels } = (await request.json()) as RequestData
     const templates = await get_templates(email, labels)
-    if (!templates) {
+    if (!templates || Object.keys(templates).length === 0) {
         return new Response("", { status: 200 })
     }
     return new Response(JSON.stringify(templates), { status: 200 })
